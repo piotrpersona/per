6eee58ca64da83c940e8eefc29f6a88dcafa016d
Piotr Persona <persona.piotr@gmail.com>
2022-10-20 20:07:57 +0200

    feat: Add comments
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- per.go
+++ per.go
@@ -2,12 +2,16 @@ package per
 
 import "fmt"
 
+// Format string to use in Error messages.
+// It MUST contain %s for message and %s or %w for error.
 var Format = "%s, err: %w"
 
+// Errorf will return error with additional formatted message if provided err is.
 func Errorf(err error, format string, a ...interface{}) error {
 	return Error(err, fmt.Sprintf(format, a...))
 }
 
+// Error will return error wrapped with message formatted with Format if provided err is not nil.
 func Error(err error, msg string) error {
 	if err != nil {
 		return fmt.Errorf(Format, msg, err)
